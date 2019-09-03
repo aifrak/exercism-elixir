3b51944d394ff505ad6a59c1b774853d3e2e7ee7
aifrak <aifrak@users.noreply.github.com>
2019-09-03 21:01:00 +0200

    Finish exercise "Rotational Cipher"

--- rotational-cipher/lib/rotational_cipher.ex
+++ rotational-cipher/lib/rotational_cipher.ex
@@ -1,4 +1,6 @@
 defmodule RotationalCipher do
+  @alphabet_length 26
+
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -8,5 +10,20 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    key = Kernel.rem(shift, 26)
+
+    text
+    |> String.to_charlist()
+    |> Enum.map(&rotate_char(&1, key))
+    |> List.to_string()
   end
+
+  @spec rotate_char(char :: char, key :: integer) :: char
+  defp rotate_char(char, key) when char >= ?a and char <= ?z, do: fix_char(char + key, ?z)
+  defp rotate_char(char, key) when char >= ?A and char <= ?Z, do: fix_char(char + key, ?Z)
+  defp rotate_char(char, _key), do: char
+
+  @spec fix_char(char :: integer, limit :: integer) :: char
+  defp fix_char(char, limit) when char > limit, do: char - @alphabet_length
+  defp fix_char(char, _limit), do: char
 end
