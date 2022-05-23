0ce93fe3d51be4ad2afbca775129decbd50bcd30
aifrak <aifrak@users.noreply.github.com>
2022-05-23 13:45:36 +0000

    Download "Log Level"

---
+++ elixir/log-level/lib/log_level.ex
@@ -0,0 +1,9 @@
+defmodule LogLevel do
+  def to_label(level, legacy?) do
+    # Please implement the to_label/2 function
+  end
+
+  def alert_recipient(level, legacy?) do
+    # Please implement the alert_recipient/2 function
+  end
+end
