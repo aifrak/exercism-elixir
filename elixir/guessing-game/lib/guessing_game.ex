bb037f42d4947e83b10e75b6edb4171ef2ffd248
aifrak <aifrak@users.noreply.github.com>
2022-05-23 15:08:08 +0000

    Complete "Guessing Game"

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,5 +1,8 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
-  end
+  def compare(_secret_number, guess \\ :no_guess)
+  def compare(_secret_number, :no_guess), do: "Make a guess"
+  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
+  def compare(secret_number, guess) when secret_number < guess, do: "Too high"
+  def compare(secret_number, guess) when secret_number > guess, do: "Too low"
 end
