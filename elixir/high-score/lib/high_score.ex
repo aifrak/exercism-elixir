27edc84e5ac29ad5d3c2dd83b9e862561f77fb29
aifrak <aifrak@users.noreply.github.com>
2022-05-24 21:41:05 +0000

    Download "High Score"

---
+++ elixir/high-score/lib/high_score.ex
@@ -0,0 +1,25 @@
+defmodule HighScore do
+  def new() do
+    # Please implement the new/0 function
+  end
+
+  def add_player(scores, name, score) do
+    # Please implement the add_player/3 function
+  end
+
+  def remove_player(scores, name) do
+    # Please implement the remove_player/2 function
+  end
+
+  def reset_score(scores, name) do
+    # Please implement the reset_score/2 function
+  end
+
+  def update_score(scores, name, score) do
+    # Please implement the update_score/3 function
+  end
+
+  def get_players(scores) do
+    # Please implement the get_players/1 function
+  end
+end
