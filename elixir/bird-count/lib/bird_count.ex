426fc2733c643c7766744f2efd2d4a5ee90104a7
aifrak <aifrak@users.noreply.github.com>
2022-05-24 11:23:53 +0000

    Complete "Bird Count"

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,26 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
-  end
+  @busy_day_threshold 5
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-  end
+  def today([]), do: nil
+  def today([first | _tail]), do: first
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-  end
+  def increment_day_count([]), do: [1]
+  def increment_day_count([count | tail]), do: [count + 1 | tail]
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([count | _tail]) when count == 0, do: true
+  def has_day_without_birds?([_count | tail]), do: has_day_without_birds?(tail)
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def total(list), do: total(list, 0)
+  def busy_days(list), do: busy_days(list, 0)
+
+  defp total([], total), do: total
+  defp total([count | tail], total), do: total(tail, count + total)
+
+  defp busy_days([], total), do: total
+
+  defp busy_days([count | tail], total) when count >= @busy_day_threshold,
+    do: busy_days(tail, total + 1)
+
+  defp busy_days([_count | tail], total), do: busy_days(tail, total)
 end
