f243f30172725ac1a33321fb8be650ba3f906c26
aifrak <aifrak@users.noreply.github.com>
2022-05-25 18:36:22 +0000

    Complete "RPG Character Sheet"

--- elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
+++ elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -1,21 +1,24 @@
 defmodule RPG.CharacterSheet do
-  def welcome() do
-    # Please implement the welcome/0 function
-  end
+  def welcome(), do: IO.puts("Welcome! Let's fill out your character sheet together.")
 
-  def ask_name() do
-    # Please implement the ask_name/0 function
-  end
+  def ask_name(), do: IO.gets("What is your character's name?\n") |> String.trim()
 
-  def ask_class() do
-    # Please implement the ask_class/0 function
-  end
+  def ask_class(), do: IO.gets("What is your character's class?\n") |> String.trim()
 
   def ask_level() do
-    # Please implement the ask_level/0 function
+    IO.gets("What is your character's level?\n")
+    |> String.trim()
+    |> String.to_integer()
   end
 
   def run() do
-    # Please implement the run/0 function
+    welcome()
+
+    %{
+      name: ask_name(),
+      class: ask_class(),
+      level: ask_level()
+    }
+    |> IO.inspect(label: "Your character")
   end
 end
