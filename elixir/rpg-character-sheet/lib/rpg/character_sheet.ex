e8369406cf4b4d3c54d2dce5884f6993905195a2
aifrak <aifrak@users.noreply.github.com>
2022-05-25 17:53:59 +0000

    Download "RPG Character Sheet"

---
+++ elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -0,0 +1,21 @@
+defmodule RPG.CharacterSheet do
+  def welcome() do
+    # Please implement the welcome/0 function
+  end
+
+  def ask_name() do
+    # Please implement the ask_name/0 function
+  end
+
+  def ask_class() do
+    # Please implement the ask_class/0 function
+  end
+
+  def ask_level() do
+    # Please implement the ask_level/0 function
+  end
+
+  def run() do
+    # Please implement the run/0 function
+  end
+end
