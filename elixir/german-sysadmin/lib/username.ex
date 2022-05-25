c8b4b8ea0ff1530385de992a23998b1fd97ed583
aifrak <aifrak@users.noreply.github.com>
2022-05-25 14:57:31 +0000

    Complete "German Sysadmin"

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,18 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+  @valid_characters [?_ | Enum.to_list(?a..?z)]
 
-    # Please implement the sanitize/1 function
+  def sanitize(username) do
+    username
+    |> Enum.map(fn char ->
+      case char do
+        ?ä -> 'ae'
+        ?ö -> 'oe'
+        ?ü -> 'ue'
+        ?ß -> 'ss'
+        char when char in @valid_characters -> char
+        _ -> ''
+      end
+    end)
+    |> List.flatten()
   end
 end
