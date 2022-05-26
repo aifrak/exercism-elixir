26e32773ca2d89c124bc8dfda5487fff7b431ada
aifrak <aifrak@users.noreply.github.com>
2022-05-26 10:25:29 +0000

    Complete "Take-A-Number"

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,22 @@
 defmodule TakeANumber do
-  def start() do
-    # Please implement the start/0 function
+  def start(), do: spawn(fn -> receive_loop(0) end)
+
+  defp receive_loop(state) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        receive_loop(state)
+
+      {:take_a_number, sender_pid} ->
+        state = state + 1
+        send(sender_pid, state)
+        receive_loop(state)
+
+      :stop ->
+        nil
+
+      _ ->
+        receive_loop(state)
+    end
   end
 end
