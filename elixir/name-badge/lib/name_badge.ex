0ded79b3cba0874c211dca6913540f12ed4206b0
aifrak <aifrak@users.noreply.github.com>
2022-05-26 08:57:48 +0000

    Complete "Name Badge"

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,5 +1,34 @@
 defmodule NameBadge do
+  @department_owner_label "owner"
+  @separator " - "
+
   def print(id, name, department) do
-    # Please implement the print/3 function
+    badge_id = badge_id(id)
+    badge_department = badge_department(department)
+
+    []
+    |> may_add_id(badge_id)
+    |> add_name(name)
+    |> add_department(badge_department)
+    |> Enum.reverse()
+    |> Enum.join(@separator)
   end
+
+  defp badge_id(id) do
+    if id, do: "[#{id}]"
+  end
+
+  defp badge_department(department) do
+    badge_department = if department, do: department, else: @department_owner_label
+
+    String.upcase(badge_department)
+  end
+
+  defp may_add_id(list, id) do
+    if id, do: ["#{id}" | list], else: list
+  end
+
+  defp add_name(list, name), do: ["#{name}" | list]
+
+  defp add_department(list, department), do: ["#{department}" | list]
 end
