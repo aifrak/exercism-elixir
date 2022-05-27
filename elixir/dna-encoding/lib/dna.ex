2b1c776d0dd3f2d8c90660ef8751146b28c6e358
aifrak <aifrak@users.noreply.github.com>
2022-05-27 11:11:17 +0000

    Complete "DNA Encoding"

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,17 +1,23 @@
 defmodule DNA do
-  def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
-  end
+  @code_size 4
 
-  def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
-  end
+  @acids %{
+    ?\s => 0b0000,
+    ?A => 0b0001,
+    ?C => 0b0010,
+    ?G => 0b0100,
+    ?T => 0b1000
+  }
 
-  def encode(dna) do
-    # Please implement the encode/1 function
-  end
+  @codes Map.new(@acids, fn {key, val} -> {val, key} end)
 
-  def decode(dna) do
-    # Please implement the decode/1 function
-  end
+  def encode_nucleotide(code_point), do: @acids[code_point]
+
+  def decode_nucleotide(encoded_code), do: @codes[encoded_code]
+
+  def encode([]), do: <<>>
+  def encode([head | tail]), do: <<encode_nucleotide(head)::@code_size, encode(tail)::bitstring>>
+
+  def decode(<<>>), do: []
+  def decode(<<head::@code_size, tail::bitstring>>), do: [decode_nucleotide(head) | decode(tail)]
 end
