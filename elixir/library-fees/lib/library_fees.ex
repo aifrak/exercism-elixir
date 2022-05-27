a0ec9aebb76f5237b32193d0b7c1c29630984d93
aifrak <aifrak@users.noreply.github.com>
2022-05-27 13:00:24 +0000

    Download "Library Fees"

---
+++ elixir/library-fees/lib/library_fees.ex
@@ -0,0 +1,25 @@
+defmodule LibraryFees do
+  def datetime_from_string(string) do
+    # Please implement the datetime_from_string/1 function
+  end
+
+  def before_noon?(datetime) do
+    # Please implement the before_noon?/1 function
+  end
+
+  def return_date(checkout_datetime) do
+    # Please implement the return_date/1 function
+  end
+
+  def days_late(planned_return_date, actual_return_datetime) do
+    # Please implement the days_late/2 function
+  end
+
+  def monday?(datetime) do
+    # Please implement the monday?/1 function
+  end
+
+  def calculate_late_fee(checkout, return, rate) do
+    # Please implement the calculate_late_fee/3 function
+  end
+end
