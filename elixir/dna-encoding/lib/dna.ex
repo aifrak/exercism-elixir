010dd0829cfd0f2a4e54d7d7925ee2b4f46d6a9e
aifrak <aifrak@users.noreply.github.com>
2022-05-27 08:46:28 +0000

    Download "DNA Encoding"

---
+++ elixir/dna-encoding/lib/dna.ex
@@ -0,0 +1,17 @@
+defmodule DNA do
+  def encode_nucleotide(code_point) do
+    # Please implement the encode_nucleotide/1 function
+  end
+
+  def decode_nucleotide(encoded_code) do
+    # Please implement the decode_nucleotide/1 function
+  end
+
+  def encode(dna) do
+    # Please implement the encode/1 function
+  end
+
+  def decode(dna) do
+    # Please implement the decode/1 function
+  end
+end
