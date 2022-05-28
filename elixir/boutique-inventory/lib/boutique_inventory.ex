f8118bf3aaf1171271a596143c76583899265618
aifrak <aifrak@users.noreply.github.com>
2022-05-28 12:42:03 +0000

    Download "Boutique Inventory"

---
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -0,0 +1,21 @@
+defmodule BoutiqueInventory do
+  def sort_by_price(inventory) do
+    # Please implement the sort_by_price/1 function
+  end
+
+  def with_missing_price(inventory) do
+    # Please implement the with_missing_price/1 function
+  end
+
+  def update_names(inventory, old_word, new_word) do
+    # Please implement the update_names/3 function
+  end
+
+  def increase_quantity(item, count) do
+    # Please implement the increase_quantity/2 function
+  end
+
+  def total_quantity(item) do
+    # Please implement the total_quantity/1 function
+  end
+end
