0e092cf2d615ee751d16d3b598df88d815bea6ae
aifrak <aifrak@users.noreply.github.com>
2022-05-28 09:45:41 +0000

    Complete "Basketball Website"

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,11 @@
 defmodule BasketballWebsite do
+  @separator "."
+
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    path
+    |> String.split(@separator)
+    |> Enum.reduce(data, & &2[&1])
   end
 
-  def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
-  end
+  def get_in_path(data, path), do: get_in(data, String.split(path, @separator))
 end
