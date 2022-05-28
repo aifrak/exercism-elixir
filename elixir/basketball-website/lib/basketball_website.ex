5a4a35571498d344889d08f4f6720fd375113930
aifrak <aifrak@users.noreply.github.com>
2022-05-28 08:03:38 +0000

    Download "Basketball Website"

---
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -0,0 +1,9 @@
+defmodule BasketballWebsite do
+  def extract_from_path(data, path) do
+    # Please implement the extract_from_path/2 function
+  end
+
+  def get_in_path(data, path) do
+    # Please implement the get_in_path/2 function
+  end
+end
