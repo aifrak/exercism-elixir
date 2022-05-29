88d90c5ff651a45660d80d7860c26e30312ec2d2
aifrak <aifrak@users.noreply.github.com>
2022-05-29 08:27:44 +0000

    Download "File Sniffer"

---
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -0,0 +1,13 @@
+defmodule FileSniffer do
+  def type_from_extension(extension) do
+    # Please implement the type_from_extension/1 function
+  end
+
+  def type_from_binary(file_binary) do
+    # Please implement the type_from_binary/1 function
+  end
+
+  def verify(file_binary, extension) do
+    # Please implement the verify/2 function
+  end
+end
