95c4cfbac3a8b6aa9d94001eee1ab2deab634508
aifrak <aifrak@users.noreply.github.com>
2022-05-31 09:09:45 +0000

    Download "Remote Control Car"

---
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -0,0 +1,23 @@
+defmodule RemoteControlCar do
+  # Please implement the struct with the specified fields
+
+  def new() do
+    # Please implement the new/0 function
+  end
+
+  def new(nickname) do
+    # Please implement the new/1 function
+  end
+
+  def display_distance(remote_car) do
+    # Please implement the display_distance/1 function
+  end
+
+  def display_battery(remote_car) do
+    # Please implement the display_battery/1 function
+  end
+
+  def drive(remote_car) do
+    # Please implement the drive/1 function
+  end
+end
