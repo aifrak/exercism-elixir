d3af0529832d800393e0966df95bf963cbe70661
aifrak <aifrak@users.noreply.github.com>
2022-05-31 10:41:37 +0000

    Complete "Remote Control Car"

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -1,23 +1,32 @@
 defmodule RemoteControlCar do
-  # Please implement the struct with the specified fields
+  @drive_meters 20
+  @battery_drain_percentage 1.0
 
-  def new() do
-    # Please implement the new/0 function
+  @enforce_keys [:nickname]
+  defstruct [
+    :nickname,
+    battery_percentage: 100,
+    distance_driven_in_meters: 0
+  ]
+
+  def new(nickname \\ "none"), do: %__MODULE__{nickname: nickname}
+
+  def display_distance(%__MODULE__{distance_driven_in_meters: meter}), do: "#{meter} meters"
+
+  def display_battery(%__MODULE__{battery_percentage: 0}), do: "Battery empty"
+  def display_battery(%__MODULE__{battery_percentage: battery}), do: "Battery at #{battery}%"
+
+  def drive(%__MODULE__{battery_percentage: 0} = remote_car), do: remote_car
+
+  def drive(%__MODULE__{} = remote_car) do
+    remote_car
+    |> Map.update!(:distance_driven_in_meters, &increase_distance/1)
+    |> Map.update!(:battery_percentage, &drain_battery/1)
   end
 
-  def new(nickname) do
-    # Please implement the new/1 function
-  end
+  defp increase_distance(distance), do: distance + @drive_meters
 
-  def display_distance(remote_car) do
-    # Please implement the display_distance/1 function
-  end
+  defp drain_battery(battery), do: battery * battery_drain_complement()
 
-  def display_battery(remote_car) do
-    # Please implement the display_battery/1 function
-  end
-
-  def drive(remote_car) do
-    # Please implement the drive/1 function
-  end
+  defp battery_drain_complement(), do: 1 - @battery_drain_percentage / 100
 end
