92cb7adaa01eedc8b00b8456f2d06bd414467fc8
aifrak <aifrak@users.noreply.github.com>
2022-05-31 08:14:32 +0000

    Download "Newsletter"

---
+++ elixir/newsletter/lib/newsletter.ex
@@ -0,0 +1,21 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    # Please implement the read_emails/1 function
+  end
+
+  def open_log(path) do
+    # Please implement the open_log/1 function
+  end
+
+  def log_sent_email(pid, email) do
+    # Please implement the log_sent_email/2 function
+  end
+
+  def close_log(pid) do
+    # Please implement the close_log/1 function
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    # Please implement the send_newsletter/3 function
+  end
+end
