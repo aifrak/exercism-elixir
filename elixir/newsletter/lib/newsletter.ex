504b8b48dcc3643dec6a8231f25d1e238c799ac9
aifrak <aifrak@users.noreply.github.com>
2022-05-31 09:07:00 +0000

    Complete "Newsletter"

--- elixir/newsletter/lib/newsletter.ex
+++ elixir/newsletter/lib/newsletter.ex
@@ -1,21 +1,27 @@
 defmodule Newsletter do
+  @email_separator "\n"
+
   def read_emails(path) do
-    # Please implement the read_emails/1 function
+    path
+    |> File.read!()
+    |> String.split(@email_separator, trim: true)
   end
 
-  def open_log(path) do
-    # Please implement the open_log/1 function
-  end
+  def open_log(path), do: File.open!(path, [:write])
 
-  def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
-  end
+  def log_sent_email(pid, email), do: IO.puts(pid, email)
 
-  def close_log(pid) do
-    # Please implement the close_log/1 function
-  end
+  def close_log(pid), do: File.close(pid)
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    log = open_log(log_path)
+
+    send_and_log_sent = &if send_fun.(&1) == :ok, do: log_sent_email(log, &1)
+
+    emails_path
+    |> read_emails()
+    |> Enum.each(&send_and_log_sent.(&1))
+
+    close_log(log)
   end
 end
