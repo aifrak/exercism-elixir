a6403bb8684d53b992bd792ff73d701ab6e317ba
aifrak <aifrak@users.noreply.github.com>
2022-06-01 15:21:00 +0000

    Complete "Boutique Suggestions"

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,18 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
-    # Please implement the get_combinations/3 function
+  @default_maximum_price 100.00
+
+  def get_combinations(tops, bottoms, options \\ []) do
+    maximum_price = Keyword.get(options, :maximum_price, @default_maximum_price)
+
+    for top <- tops,
+        bottom <- bottoms,
+        combination = {top, bottom},
+        different_color?(combination) and total_price(combination) < maximum_price do
+      combination
+    end
   end
+
+  defp different_color?({top, bottom}), do: top.base_color != bottom.base_color
+
+  defp total_price({top, bottom}), do: top.price + bottom.price
 end
