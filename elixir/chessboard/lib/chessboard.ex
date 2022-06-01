559a9d459ef3b77ee6f432d15c9011745e1f9b34
aifrak <aifrak@users.noreply.github.com>
2022-06-01 12:27:30 +0000

    Download "Chessboard"

---
+++ elixir/chessboard/lib/chessboard.ex
@@ -0,0 +1,17 @@
+defmodule Chessboard do
+  def rank_range do
+    # Please implement the rank_range/0 function
+  end
+
+  def file_range do
+    # Please implement the file_range/0 function
+  end
+
+  def ranks do
+    # Please implement the ranks/0 function
+  end
+
+  def files do
+    # Please implement the files/0 function
+  end
+end
