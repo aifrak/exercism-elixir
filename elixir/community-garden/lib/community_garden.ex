e81ebd435f7df5b784faed7c44e9fb0f2dff4abe
aifrak <aifrak@users.noreply.github.com>
2022-06-02 19:07:09 +0000

    Complete "Community Garden"

--- elixir/community-garden/lib/community_garden.ex
+++ elixir/community-garden/lib/community_garden.ex
@@ -2,26 +2,73 @@
 defmodule Plot do
   @enforce_keys [:plot_id, :registered_to]
   defstruct [:plot_id, :registered_to]
+
+  def new(id, register_to), do: %__MODULE__{plot_id: id, registered_to: register_to}
+end
+
+defmodule Plot.Map do
+  def new, do: %{}
+
+  def list(plots), do: Map.values(plots)
+
+  def add(plots, plot), do: Map.put_new(plots, plot.plot_id, plot)
+
+  def get(plots, id) do
+    plot = plots[id]
+
+    if is_struct(plot, Plot),
+      do: plot,
+      else: {:not_found, "plot is unregistered"}
+  end
+
+  def delete(plots, plot_id), do: Map.delete(plots, plot_id)
 end
 
 defmodule CommunityGarden do
-  def start(opts) do
-    # Please implement the start/1 function
-  end
+  use Agent
 
-  def list_registrations(pid) do
-    # Please implement the list_registrations/1 function
-  end
+  @initial_next_id 1
+
+  def start(_opts \\ []), do: Agent.start(&init_state/0)
+
+  def list_registrations(pid), do: Agent.get(pid, &Plot.Map.list(&1.plots))
 
   def register(pid, register_to) do
-    # Please implement the register/2 function
+    pid
+    |> next_id()
+    |> Plot.new(register_to)
+    |> tap(&add(pid, &1))
   end
 
   def release(pid, plot_id) do
-    # Please implement the release/2 function
+    Agent.update(pid, fn state ->
+      updated_plots = Plot.Map.delete(state.plots, plot_id)
+
+      %{state | plots: updated_plots}
+    end)
   end
 
-  def get_registration(pid, plot_id) do
-    # Please implement the get_registration/2 function
+  def get_registration(pid, plot_id), do: Agent.get(pid, &Plot.Map.get(&1.plots, plot_id))
+
+  defp init_state, do: %{plots: Plot.Map.new(), next_id: @initial_next_id}
+
+  defp add(pid, plot) do
+    Agent.update(pid, fn state ->
+      updated_plots = Plot.Map.add(state.plots, plot)
+
+      %{state | plots: updated_plots}
+    end)
+  end
+
+  defp next_id(pid) do
+    %{next_id: id} =
+      Agent.get_and_update(pid, fn state ->
+        new_next_id = state.next_id + 1
+        new_state = %{state | next_id: new_next_id}
+
+        {state, new_state}
+      end)
+
+    id
   end
 end
