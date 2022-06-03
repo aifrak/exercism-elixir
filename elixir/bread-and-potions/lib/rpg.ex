adeaac240e69ee4e27e665b41f36deaeb7eb2d21
aifrak <aifrak@users.noreply.github.com>
2022-06-03 10:21:58 +0000

    Complete "Bread And Potions"

--- elixir/bread-and-potions/lib/rpg.ex
+++ elixir/bread-and-potions/lib/rpg.ex
@@ -20,4 +20,33 @@ defmodule RPG do
   end
 
   # Add code to define the protocol and its implementations below here...
+  defprotocol Edible do
+    def eat(item, character)
+  end
+
+  defimpl Edible, for: RPG.LoafOfBread do
+    @health_regen 5
+
+    def eat(_item, %Character{} = character) do
+      character
+      |> Map.update!(:health, &(&1 + @health_regen))
+      |> then(&{nil, &1})
+    end
+  end
+
+  defimpl Edible, for: RPG.ManaPotion do
+    def eat(%ManaPotion{} = item, %Character{} = character) do
+      character
+      |> Map.update!(:mana, &(&1 + item.strength))
+      |> then(&{%EmptyBottle{}, &1})
+    end
+  end
+
+  defimpl Edible, for: RPG.Poison do
+    def eat(_item, %Character{} = character) do
+      character
+      |> struct(health: 0)
+      |> then(&{%EmptyBottle{}, &1})
+    end
+  end
 end
