09ad9c1f0f3ce571531769b5c02ec2f3460b72fe
aifrak <aifrak@users.noreply.github.com>
2022-06-04 07:30:29 +0000

    Download "RPN Calculator"

---
+++ elixir/rpn-calculator/lib/rpn_calculator.ex
@@ -0,0 +1,13 @@
+defmodule RPNCalculator do
+  def calculate!(stack, operation) do
+    # Please implement the calculate!/2 function
+  end
+
+  def calculate(stack, operation) do
+    # Please implement the calculate/2 function
+  end
+
+  def calculate_verbose(stack, operation) do
+    # Please implement the calculate_verbose/2 function
+  end
+end
