e0374f521724b68a9dd31bfab8359c45bf9119b1
aifrak <aifrak@users.noreply.github.com>
2022-06-05 15:53:46 +0000

    Download "RPN Calculator Inspection"

---
+++ elixir/rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
@@ -0,0 +1,17 @@
+defmodule RPNCalculatorInspection do
+  def start_reliability_check(calculator, input) do
+    # Please implement the start_reliability_check/2 function
+  end
+
+  def await_reliability_check_result(%{pid: pid, input: input}, results) do
+    # Please implement the await_reliability_check_result/2 function
+  end
+
+  def reliability_check(calculator, inputs) do
+    # Please implement the reliability_check/2 function
+  end
+
+  def correctness_check(calculator, inputs) do
+    # Please implement the correctness_check/2 function
+  end
+end
