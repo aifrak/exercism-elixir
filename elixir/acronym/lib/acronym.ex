8d71cd6435180c3da3fca7ebdff9ea99b5b65a2b
aifrak <aifrak@users.noreply.github.com>
2022-06-07 09:57:25 +0000

    Complete "Acronym"

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -1,9 +1,21 @@
 defmodule Acronym do
+  @separators [" ", "-", "_", ","]
+
   @doc """
   Generate an acronym from a string.
   "This is a string" => "TIAS"
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.splitter(@separators)
+    |> Stream.map(&initial_letter/1)
+    |> Enum.join()
+  end
+
+  defp initial_letter(string) do
+    string
+    |> String.capitalize()
+    |> String.first()
   end
 end
