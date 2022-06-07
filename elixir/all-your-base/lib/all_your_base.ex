b5566b2b55494c74a12cfb3e720da37d21fae2ce
aifrak <aifrak@users.noreply.github.com>
2022-06-07 13:09:48 +0000

    Download "All Your Base"

---
+++ elixir/all-your-base/lib/all_your_base.ex
@@ -0,0 +1,10 @@
+defmodule AllYourBase do
+  @doc """
+  Given a number in input base, represented as a sequence of digits, converts it to output base,
+  or returns an error tuple if either of the bases are less than 2
+  """
+
+  @spec convert(list, integer, integer) :: {:ok, list} | {:error, String.t()}
+  def convert(digits, input_base, output_base) do
+  end
+end
