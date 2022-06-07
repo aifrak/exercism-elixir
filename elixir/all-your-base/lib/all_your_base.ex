a8a1d3bf924a77a3c73e4c6ebf3183309b2ca809
aifrak <aifrak@users.noreply.github.com>
2022-06-07 18:31:46 +0000

    Complete "All Your Base"

--- elixir/all-your-base/lib/all_your_base.ex
+++ elixir/all-your-base/lib/all_your_base.ex
@@ -5,6 +5,46 @@ defmodule AllYourBase do
   """
 
   @spec convert(list, integer, integer) :: {:ok, list} | {:error, String.t()}
+  def convert(_, _, output_base) when output_base < 2, do: {:error, "output base must be >= 2"}
+  def convert(_, input_base, _) when input_base < 2, do: {:error, "input base must be >= 2"}
+  def convert([], _, _), do: {:ok, [0]}
+
   def convert(digits, input_base, output_base) do
+    if Enum.all?(digits, &valid_digit(&1, input_base)) do
+      {:ok, do_convert(digits, input_base, output_base)}
+    else
+      {:error, "all digits must be >= 0 and < input base"}
+    end
+  end
+
+  defp do_convert(digits, input_base, output_base) do
+    digits
+    |> to_base10_number(input_base)
+    |> from_base10_number(output_base)
+  end
+
+  defp valid_digit(digit, base), do: digit >= 0 and digit < base
+
+  defp to_base10_number(digits, base) do
+    highest_exponent = length(digits) - 1
+
+    digits
+    |> Enum.reduce({0, highest_exponent}, fn digit, {total, exponent} ->
+      base10_digit = digit * Integer.pow(base, exponent)
+
+      {total + base10_digit, exponent - 1}
+    end)
+    |> then(fn {result, _} -> result end)
+  end
+
+  defp from_base10_number(0, _), do: [0]
+
+  defp from_base10_number(number, base) do
+    number
+    |> Stream.unfold(fn
+      0 -> nil
+      num -> {rem(num, base), div(num, base)}
+    end)
+    |> Enum.reverse()
   end
 end
