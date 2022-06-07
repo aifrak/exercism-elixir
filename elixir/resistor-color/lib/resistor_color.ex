b01a7fa47a33a9ddeb80764512f9407ab83570f0
aifrak <aifrak@users.noreply.github.com>
2022-06-07 07:36:17 +0000

    Complete "Resistor Color"

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -1,8 +1,9 @@
 defmodule ResistorColor do
+  @colors [:black, :brown, :red, :orange, :yellow, :green, :blue, :violet, :grey, :white]
+
   @doc """
   Return the value of a color band
   """
   @spec code(atom) :: integer()
-  def code(color) do
-  end
+  def code(color), do: Enum.find_index(@colors, &(&1 == color))
 end
