4775c893213950c6ddcc5046be13b4e7e8ca86d9
aifrak <aifrak@users.noreply.github.com>
2022-06-08 08:48:02 +0000

    Complete "Anagram"

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,5 +4,21 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    downcased_base = String.downcase(base)
+    sorted_base = sort(downcased_base)
+
+    Enum.filter(candidates, fn candidate ->
+      downcased_candidate = String.downcase(candidate)
+
+      downcased_candidate != downcased_base &&
+        sort(downcased_candidate) == sorted_base
+    end)
+  end
+
+  defp sort(string) do
+    string
+    |> String.graphemes()
+    |> Enum.sort()
+    |> Enum.join()
   end
 end
