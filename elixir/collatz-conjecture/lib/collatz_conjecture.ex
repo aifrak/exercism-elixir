20ac5a6b5cdf8f2a4ae346cde2ceff5940db9bf5
aifrak <aifrak@users.noreply.github.com>
2022-06-08 09:22:18 +0000

    Download "Collatz Conjecture"

---
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -0,0 +1,11 @@
+defmodule CollatzConjecture do
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+  @spec calc(input :: pos_integer()) :: non_neg_integer()
+  def calc(input) do
+  end
+end
