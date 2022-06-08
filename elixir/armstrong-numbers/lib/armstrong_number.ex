bcb0615144b6c18b4ebe6854bc57b8cd2c2ec30b
aifrak <aifrak@users.noreply.github.com>
2022-06-08 09:06:54 +0000

    Download "Armstrong Numbers"

---
+++ elixir/armstrong-numbers/lib/armstrong_number.ex
@@ -0,0 +1,9 @@
+defmodule ArmstrongNumber do
+  @moduledoc """
+  Provides a way to validate whether or not a number is an Armstrong number
+  """
+
+  @spec valid?(integer) :: boolean
+  def valid?(number) do
+  end
+end
