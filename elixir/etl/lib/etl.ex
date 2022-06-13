a72b4a193e9db4f991162385d086b4988b1f5373
aifrak <aifrak@users.noreply.github.com>
2022-06-13 16:06:45 +0000

    Download "ETL"

---
+++ elixir/etl/lib/etl.ex
@@ -0,0 +1,13 @@
+defmodule ETL do
+  @doc """
+  Transforms an old Scrabble score system to a new one.
+
+  ## Examples
+
+    iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
+    %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+  end
+end
