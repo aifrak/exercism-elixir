6c9b9e2fc3c555e6233931daf8408476faf8a0b2
aifrak <aifrak@users.noreply.github.com>
2022-06-13 20:57:14 +0000

    Download "Hamming"

---
+++ elixir/hamming/lib/hamming.ex
@@ -0,0 +1,13 @@
+defmodule Hamming do
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+  iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
+  {:ok, 4}
+  """
+  @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
+  def hamming_distance(strand1, strand2) do
+  end
+end
