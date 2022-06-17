c66e6b18975b27f2358a560e62fdfd3bb5e64f20
aifrak <aifrak@users.noreply.github.com>
2022-06-17 14:53:33 +0000

    Download "Run Length Encoding"

---
+++ elixir/run-length-encoding/lib/run_length_encoder.ex
@@ -0,0 +1,16 @@
+defmodule RunLengthEncoder do
+  @doc """
+  Generates a string where consecutive elements are represented as a data value and count.
+  "AABBBCCCC" => "2A3B4C"
+  For this example, assume all input are strings, that are all uppercase letters.
+  It should also be able to reconstruct the data into its original form.
+  "2A3B4C" => "AABBBCCCC"
+  """
+  @spec encode(String.t()) :: String.t()
+  def encode(string) do
+  end
+
+  @spec decode(String.t()) :: String.t()
+  def decode(string) do
+  end
+end
