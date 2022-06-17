d38c20fd97b96d581004563b89375fda844bf46b
aifrak <aifrak@users.noreply.github.com>
2022-06-17 13:01:52 +0000

    Complete "Resistor Color Duo"

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -1,8 +1,25 @@
 defmodule ResistorColorDuo do
+  @band_colors %{
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
   @doc """
   Calculate a resistance value from two colors
   """
   @spec value(colors :: [atom]) :: integer
   def value(colors) do
+    colors
+    |> Stream.map(&(@band_colors[&1]))
+    |> Enum.take(2)
+    |> Integer.undigits()
   end
 end
