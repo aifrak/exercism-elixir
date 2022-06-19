a001a5dc79bab7566c4a0ad78f4087b2560bc90c
aifrak <aifrak@users.noreply.github.com>
2022-06-19 12:39:00 +0000

    Download "Space Age"

---
+++ elixir/space-age/lib/space_age.ex
@@ -0,0 +1,19 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet', or an error if 'planet' is not a planet.
+  """
+  @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
+  def age_on(planet, seconds) do
+  end
+end
