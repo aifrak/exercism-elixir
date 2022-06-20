581207d061f3d5950b4c0f437431efb6d9321a6c
aifrak <aifrak@users.noreply.github.com>
2022-06-20 14:59:38 +0000

    Complete "Sum Of Multiples"

--- elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -4,5 +4,16 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    factors
+    |> Stream.filter(&(&1 > 0))
+    |> Stream.flat_map(&multiples(limit, &1))
+    |> Stream.uniq()
+    |> Enum.sum()
+  end
+
+  defp multiples(limit, factor) do
+    factor
+    |> Stream.iterate(&(&1 + factor))
+    |> Enum.take_while(&(&1 < limit))
   end
 end
