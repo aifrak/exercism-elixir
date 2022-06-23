c0d2864e010c0e7dd1c4dbcceb7500bf8f432e2b
aifrak <aifrak@users.noreply.github.com>
2022-06-23 15:15:07 +0000

    Complete "Atbash Cipher"

--- elixir/atbash-cipher/lib/atbash.ex
+++ elixir/atbash-cipher/lib/atbash.ex
@@ -1,4 +1,13 @@
 defmodule Atbash do
+  @plain Enum.map(?a..?z, &<<&1>>)
+  @cipher Enum.reverse(@plain)
+  @digits Enum.map(?0..?9, &<<&1>>)
+
+  @atbash Enum.zip(@plain, @cipher) |> Map.new()
+
+  @cipher_separator " "
+  @group_size 5
+
   @doc """
   Encode a given plaintext to the corresponding ciphertext
 
@@ -9,9 +18,36 @@ defmodule Atbash do
   """
   @spec encode(String.t()) :: String.t()
   def encode(plaintext) do
+    plaintext
+    |> open()
+    |> IO.binstream(1)
+    |> Stream.map(&String.downcase/1)
+    |> Stream.map(&convert/1)
+    |> Stream.reject(&(&1 == nil))
+    |> Stream.chunk_every(@group_size)
+    |> Enum.join(@cipher_separator)
   end
 
   @spec decode(String.t()) :: String.t()
   def decode(cipher) do
+    cipher
+    |> open()
+    |> IO.binstream(1)
+    |> Stream.map(&convert/1)
+    |> Stream.reject(&(&1 == nil))
+    |> Enum.join()
+  end
+
+  defp convert(char) do
+    cond do
+      char in @digits -> char
+      char in @plain -> @atbash[char]
+      true -> nil
+    end
+  end
+
+  defp open(str) do
+    {:ok, pid} = StringIO.open(str)
+    pid
   end
 end
