c75e27f21d3322079c1757d06266dcce6efe7f61
aifrak <aifrak@users.noreply.github.com>
2022-06-23 08:47:51 +0000

    Download "Atbash Cipher"

---
+++ elixir/atbash-cipher/lib/atbash.ex
@@ -0,0 +1,17 @@
+defmodule Atbash do
+  @doc """
+  Encode a given plaintext to the corresponding ciphertext
+
+  ## Examples
+
+  iex> Atbash.encode("completely insecure")
+  "xlnko vgvob rmhvx fiv"
+  """
+  @spec encode(String.t()) :: String.t()
+  def encode(plaintext) do
+  end
+
+  @spec decode(String.t()) :: String.t()
+  def decode(cipher) do
+  end
+end
