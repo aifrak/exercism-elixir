735db9417730fe995a0134c5443cb503bc96458e
aifrak <aifrak@users.noreply.github.com>
2022-06-24 14:27:01 +0000

    Complete "Binary Search"

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -18,6 +18,17 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
-  def search(numbers, key) do
+  def search(numbers, key), do: do_search(numbers, key, 0, tuple_size(numbers) - 1)
+
+  defp do_search(_, _, l, r) when l > r, do: :not_found
+
+  defp do_search(numbers, key, l, r) do
+    m = floor((l + r) / 2)
+
+    cond do
+      elem(numbers, m) < key -> do_search(numbers, key, m + 1, r)
+      elem(numbers, m) > key -> do_search(numbers, key, l, m - 1)
+      true -> {:ok, m}
+    end
   end
 end
