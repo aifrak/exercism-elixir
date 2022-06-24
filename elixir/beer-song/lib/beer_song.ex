0e5951e0fd8c1b4803b26acc17a7a0fd13f781e7
aifrak <aifrak@users.noreply.github.com>
2022-06-24 12:59:20 +0000

    Complete "Beer Song"

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -1,17 +1,33 @@
 defmodule BeerSong do
+  import String, only: [capitalize: 1]
+
   @doc """
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
   def verse(number) do
-    # Your implementation here...
+    """
+    #{capitalize(bottles(number))} of beer on the wall, #{bottles(number)} of beer.
+    #{action(number)}, #{bottles(number - 1)} of beer on the wall.
+    """
   end
+
+  defp bottles(number) do
+    case number do
+      0 -> "no more bottles"
+      1 -> "#{number} bottle"
+      number when number > 1 -> "#{number} bottles"
+      number when number < 1 -> "99 bottles"
+    end
+  end
+
+  defp action(0), do: "Go to the store and buy some more"
+  defp action(1), do: "Take it down and pass it around"
+  defp action(number) when number > 1, do: "Take one down and pass it around"
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
-  def lyrics(range) do
-    # Your implementation here...
-  end
+  def lyrics(range \\ 99..00), do: Enum.map_join(range, "\n", &verse/1)
 end
