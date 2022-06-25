1d57963294b2191396ab90e03a3ac42eca7ae99b
aifrak <aifrak@users.noreply.github.com>
2022-06-25 14:43:42 +0000

    Complete "Bob"

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,5 +1,20 @@
 defmodule Bob do
   @spec hey(String.t()) :: String.t()
   def hey(input) do
+    input = String.trim(input)
+
+    cond do
+      silent?(input) -> "Fine. Be that way!"
+      yell?(input) and ask?(input) -> "Calm down, I know what I'm doing!"
+      yell?(input) -> "Whoa, chill out!"
+      ask?(input) -> "Sure."
+      true -> "Whatever."
+    end
   end
+
+  defp yell?(input), do: input == String.upcase(input) and String.match?(input, ~r/[A-ZА-Я]+/)
+
+  defp ask?(input), do: String.ends_with?(input, "?")
+
+  defp silent?(input), do: input == ""
 end
