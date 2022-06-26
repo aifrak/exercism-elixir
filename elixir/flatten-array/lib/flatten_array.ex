07f2b251b41f0520fac19bcd3ad28344be010637
aifrak <aifrak@users.noreply.github.com>
2022-06-26 08:46:00 +0000

    Download "Flatten Array"

---
+++ elixir/flatten-array/lib/flatten_array.ex
@@ -0,0 +1,18 @@
+defmodule FlattenArray do
+  @doc """
+    Accept a list and return the list flattened without nil values.
+
+    ## Examples
+
+      iex> FlattenArray.flatten([1, [2], 3, nil])
+      [1,2,3]
+
+      iex> FlattenArray.flatten([nil, nil])
+      []
+
+  """
+
+  @spec flatten(list) :: list
+  def flatten(list) do
+  end
+end
