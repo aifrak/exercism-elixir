074e731ca7c2e2a30b8beb7ccf2e46cc0c7f5039
aifrak <aifrak@users.noreply.github.com>
2022-06-27 07:46:54 +0000

    Download "Gigasecond"

---
+++ elixir/gigasecond/lib/gigasecond.ex
@@ -0,0 +1,9 @@
+defmodule Gigasecond do
+  @doc """
+  Calculate a date one billion seconds after an input date.
+  """
+  @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
+          {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
+  def from({{year, month, day}, {hours, minutes, seconds}}) do
+  end
+end
