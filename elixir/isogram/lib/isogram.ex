be2f914b364fe81c5a51c33676487cfa8e0cb1eb
aifrak <aifrak@users.noreply.github.com>
2022-06-29 14:05:57 +0000

    Complete "Isogram"

--- elixir/isogram/lib/isogram.ex
+++ elixir/isogram/lib/isogram.ex
@@ -3,6 +3,16 @@ defmodule Isogram do
   Determines if a word or sentence is an isogram
   """
   @spec isogram?(String.t()) :: boolean
-  def isogram?(sentence) do
+  def isogram?(sentence), do: isogram?(sentence, [])
+  defp isogram?(<<>>, _), do: true
+
+  defp isogram?(<<char, tail::binary>>, acc) do
+    <<char>> = String.downcase(<<char>>)
+
+    cond do
+      char in acc -> false
+      char in ?a..?z -> isogram?(tail, [char | acc])
+      true -> isogram?(tail, acc)
+    end
   end
 end
