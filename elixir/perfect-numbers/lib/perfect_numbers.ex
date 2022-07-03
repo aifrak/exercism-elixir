a2f2661a7f1f55e566a29099c7a1be6f0d8e592f
aifrak <aifrak@users.noreply.github.com>
2022-07-03 11:00:27 +0000

    Complete "Perfect Numbers"

--- elixir/perfect-numbers/lib/perfect_numbers.ex
+++ elixir/perfect-numbers/lib/perfect_numbers.ex
@@ -1,4 +1,6 @@
 defmodule PerfectNumbers do
+  defguardp is_divisor(divisor, dividend) when rem(dividend, divisor) == 0
+
   @doc """
   Determine the aliquot sum of the given `number`, by summing all the factors
   of `number`, aside from `number` itself.
@@ -10,6 +12,21 @@ defmodule PerfectNumbers do
   :deficient if the aliquot sum is less than `number`
   """
   @spec classify(number :: integer) :: {:ok, atom} | {:error, String.t()}
-  def classify(number) do
+  def classify(number) when number > 0 do
+    number
+    |> aliquot_sum()
+    |> then(&{:ok, do_classify(&1, number)})
   end
+
+  def classify(_), do: {:error, "Classification is only possible for natural numbers."}
+
+  def aliquot_sum(number) do
+    1..ceil(number / 2)
+    |> Stream.filter(&(is_divisor(&1, number) && number != &1))
+    |> Enum.sum()
+  end
+
+  defp do_classify(sum, number) when sum > number, do: :abundant
+  defp do_classify(sum, number) when sum < number, do: :deficient
+  defp do_classify(_, _), do: :perfect
 end
