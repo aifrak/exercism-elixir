238a18fd62bf0ee12a0303249db458c49a519b51
aifrak <aifrak@users.noreply.github.com>
2022-07-03 16:24:37 +0000

    Complete "Prime Factors"

--- elixir/prime-factors/lib/prime_factors.ex
+++ elixir/prime-factors/lib/prime_factors.ex
@@ -1,4 +1,6 @@
 defmodule PrimeFactors do
+  defguardp is_divisible(dividend, divisor) when rem(dividend, divisor) == 0
+
   @doc """
   Compute the prime factors for 'number'.
 
@@ -8,6 +10,12 @@ defmodule PrimeFactors do
   The prime factors of 'number' will be ordered lowest to highest.
   """
   @spec factors_for(pos_integer) :: [pos_integer]
-  def factors_for(number) do
-  end
+  def factors_for(number), do: do_factors_for(number, 2)
+
+  defp do_factors_for(1, _), do: []
+
+  defp do_factors_for(number, divisor) when is_divisible(number, divisor),
+    do: [divisor | do_factors_for(round(number / divisor), divisor)]
+
+  defp do_factors_for(number, divisor), do: do_factors_for(number, divisor + 1)
 end
