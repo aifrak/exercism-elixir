65200d969244fb3af49465005e72d417a69a243c
aifrak <aifrak@users.noreply.github.com>
2022-07-04 09:06:57 +0000

    Complete "Proverb"

--- elixir/proverb/lib/proverb.ex
+++ elixir/proverb/lib/proverb.ex
@@ -3,6 +3,12 @@ defmodule Proverb do
   Generate a proverb from a list of strings.
   """
   @spec recite(strings :: [String.t()]) :: String.t()
+  def recite([]), do: ""
+
   def recite(strings) do
+    strings
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map_join("", fn [s1, s2] -> "For want of a #{s1} the #{s2} was lost.\n" end)
+    |> Kernel.<>("And all for the want of a #{List.first(strings)}.\n")
   end
 end
