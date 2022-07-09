d522f2e6dcecded863ea7d74a81ae3bd4362bc46
aifrak <aifrak@users.noreply.github.com>
2022-07-09 08:14:57 +0000

    Download "Allergies"

---
+++ elixir/allergies/lib/allergies.ex
@@ -0,0 +1,15 @@
+defmodule Allergies do
+  @doc """
+  List the allergies for which the corresponding flag bit is true.
+  """
+  @spec list(non_neg_integer) :: [String.t()]
+  def list(flags) do
+  end
+
+  @doc """
+  Returns whether the corresponding flag bit in 'flags' is set for the item.
+  """
+  @spec allergic_to?(non_neg_integer, String.t()) :: boolean
+  def allergic_to?(flags, item) do
+  end
+end
