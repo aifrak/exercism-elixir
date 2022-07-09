d197aa3297f2a73e9ccc758d918f2b0e016e236f
aifrak <aifrak@users.noreply.github.com>
2022-07-09 10:21:33 +0000

    Complete "Allergies"

--- elixir/allergies/lib/allergies.ex
+++ elixir/allergies/lib/allergies.ex
@@ -1,15 +1,28 @@
 defmodule Allergies do
+  @scores %{
+    "eggs" => 1,
+    "peanuts" => 2,
+    "shellfish" => 4,
+    "strawberries" => 8,
+    "tomatoes" => 16,
+    "chocolate" => 32,
+    "pollen" => 64,
+    "cats" => 128
+  }
+
   @doc """
   List the allergies for which the corresponding flag bit is true.
   """
   @spec list(non_neg_integer) :: [String.t()]
   def list(flags) do
+    @scores
+    |> Map.keys()
+    |> Enum.filter(&allergic_to?(flags, &1))
   end
 
   @doc """
   Returns whether the corresponding flag bit in 'flags' is set for the item.
   """
   @spec allergic_to?(non_neg_integer, String.t()) :: boolean
-  def allergic_to?(flags, item) do
-  end
+  def allergic_to?(flags, item), do: Bitwise.band(flags, @scores[item]) > 0
 end
