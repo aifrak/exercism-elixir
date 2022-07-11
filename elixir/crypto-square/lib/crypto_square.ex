c52918332307c3a12309fbb5ec1fb5058ff8d78d
aifrak <aifrak@users.noreply.github.com>
2022-07-11 16:48:05 +0000

    Complete "Crypto Square"

--- elixir/crypto-square/lib/crypto_square.ex
+++ elixir/crypto-square/lib/crypto_square.ex
@@ -7,6 +7,41 @@ defmodule CryptoSquare do
     "ac bd"
   """
   @spec encode(String.t()) :: String.t()
+  def encode(""), do: ""
+
   def encode(str) do
+    message = normalize(str)
+    cols_nb = columns_number(message)
+
+    message
+    |> String.graphemes()
+    |> Enum.chunk_every(cols_nb)
+    |> Enum.map(&pad_trailing(&1, cols_nb, " "))
+    |> Enum.zip_with(& &1)
+    |> Enum.join(" ")
   end
+
+  defp normalize(str), do: str |> String.replace(~r/[^[:alnum:]]/, "") |> String.downcase()
+
+  defp columns_number(message) do
+    message_length = String.length(message)
+
+    message_length
+    |> :math.sqrt()
+    |> round()
+    |> may_adjust_columns_number(message_length)
+  end
+
+  defp may_adjust_columns_number(cols_nb, message_length),
+    do: if(cols_nb * cols_nb >= message_length, do: cols_nb, else: cols_nb + 1)
+
+  defp pad_trailing(list, cols_nb, padding), do: pad(list, cols_nb, padding, 0, [])
+
+  defp pad(_, cols_nb, _, cols_nb, acc), do: Enum.reverse(acc)
+
+  defp pad([], cols_nb, padding, count, acc),
+    do: pad([], cols_nb, padding, count + 1, [padding | acc])
+
+  defp pad([h | t], cols_nb, padding, count, acc),
+    do: pad(t, cols_nb, padding, count + 1, [h | acc])
 end
