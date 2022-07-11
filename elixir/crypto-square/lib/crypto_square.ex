ab5b86d278974fad0a74c3153c8b68b4e7e05914
aifrak <aifrak@users.noreply.github.com>
2022-07-11 08:06:42 +0000

    Download "Crypto Square"

---
+++ elixir/crypto-square/lib/crypto_square.ex
@@ -0,0 +1,12 @@
+defmodule CryptoSquare do
+  @doc """
+  Encode string square methods
+  ## Examples
+
+    iex> CryptoSquare.encode("abcd")
+    "ac bd"
+  """
+  @spec encode(String.t()) :: String.t()
+  def encode(str) do
+  end
+end
