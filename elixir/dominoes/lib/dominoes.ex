c7a604ae911da483d1263007a82dd86fbd7b893c
aifrak <aifrak@users.noreply.github.com>
2022-07-15 19:50:26 +0000

    Complete "Dominoes"

--- elixir/dominoes/lib/dominoes.ex
+++ elixir/dominoes/lib/dominoes.ex
@@ -6,6 +6,27 @@ defmodule Dominoes do
   possible to make a full chain
   """
   @spec chain?(dominoes :: [domino]) :: boolean
-  def chain?(dominoes) do
+  def chain?([]), do: true
+  def chain?([{a, a}]), do: true
+  def chain?([{_, _}]), do: false
+
+  def chain?(dominoes), do: do_chain?(dominoes, MapSet.new(), [])
+
+  defp do_chain?([{h1, h2}], halves, _),
+    do: if(h1 in halves and h2 in halves, do: true, else: false)
+
+  defp do_chain?([{h1, h2} = h | t] = all, halves, solos) do
+    cond do
+      Enum.empty?(halves) or h1 in halves or h2 in halves ->
+        do_chain?(t, MapSet.union(halves, MapSet.new([h1, h2])), Enum.reject(solos, & &1))
+
+      all == solos ->
+        false
+
+      true ->
+        do_chain?(t ++ [h], halves, may_add_to_solos(h, solos))
+    end
   end
+
+  defp may_add_to_solos(domino, solos), do: if(domino in solos, do: solos, else: [domino | solos])
 end
