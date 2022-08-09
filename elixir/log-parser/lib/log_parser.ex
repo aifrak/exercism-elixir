09d28270680e6db9c0474b9e846b91db02485ebb
aifrak <aifrak@users.noreply.github.com>
2022-08-09 15:49:49 +0000

    Complete "Log Parser"

--- elixir/log-parser/lib/log_parser.ex
+++ elixir/log-parser/lib/log_parser.ex
@@ -1,17 +1,16 @@
 defmodule LogParser do
-  def valid_line?(line) do
-    # Please implement the valid_line?/1 function
-  end
+  def valid_line?(line), do: line =~ ~r/^\[(DEBUG|INFO|WARNING|ERROR)\]/
 
-  def split_line(line) do
-    # Please implement the split_line/1 function
-  end
+  def split_line(line), do: Regex.split(~r/<[~*=-]*>/, line)
 
-  def remove_artifacts(line) do
-    # Please implement the remove_artifacts/1 function
-  end
+  def remove_artifacts(line), do: Regex.replace(~r/end-of-line\d+/i, line, "")
 
   def tag_with_user_name(line) do
-    # Please implement the tag_with_user_name/1 function
+    ~r/User\s+(\S+)/
+    |> Regex.run(line, capture: :all_but_first)
+    |> do_tag_with_user_name(line)
   end
+
+  defp do_tag_with_user_name([user], line), do: "[USER] #{user} #{line}"
+  defp do_tag_with_user_name(_, line), do: line
 end
