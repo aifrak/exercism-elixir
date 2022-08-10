5a20af3ee068f32829cb4e8d65ef5ae17a541284
aifrak <aifrak@users.noreply.github.com>
2022-08-10 10:31:14 +0000

    Complete "Largest Series Product"

--- elixir/largest-series-product/lib/series.ex
+++ elixir/largest-series-product/lib/series.ex
@@ -3,6 +3,27 @@ defmodule Series do
   Finds the largest product of a given number of consecutive numbers in a given string of numbers.
   """
   @spec largest_product(String.t(), non_neg_integer) :: non_neg_integer
+  def largest_product(_, 0), do: 1
+
   def largest_product(number_string, size) do
+    with :ok <- validate_size(size),
+         :ok <- validate_lengths(number_string, size) do
+      number_string
+      |> String.graphemes()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(size, 1, :discard)
+      |> Enum.map(&(Enum.dedup(&1) |> Enum.product()))
+      |> Enum.max()
+    end
+  end
+
+  defp validate_size(size) do
+    if size >= 0, do: :ok, else: raise(ArgumentError, "Span size must be positive")
+  end
+
+  defp validate_lengths(number_string, size) do
+    if String.length(number_string) >= size,
+      do: :ok,
+      else: raise(ArgumentError, "Number of digits must be less than or equal to span size")
   end
 end
