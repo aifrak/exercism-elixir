48c7bbb183a05c4d81f4cdad6cb15541f3cfea95
aifrak <aifrak@users.noreply.github.com>
2022-08-12 19:12:19 +0000

    Complete "Parallel Letter Frequency"

--- elixir/parallel-letter-frequency/lib/frequency.ex
+++ elixir/parallel-letter-frequency/lib/frequency.ex
@@ -8,5 +8,23 @@ defmodule Frequency do
   """
   @spec frequency([String.t()], pos_integer) :: map
   def frequency(texts, workers) do
+    texts
+    |> Task.async_stream(&frequency/1, max_concurrency: workers)
+    |> Enum.reduce(%{}, fn {:ok, frequencies}, acc -> sum_frequencies(frequencies, acc) end)
   end
+
+  defp frequency(text) do
+    {:ok, pid} = StringIO.open(text)
+
+    pid
+    |> IO.stream(1)
+    |> Stream.map(&String.downcase/1)
+    |> Stream.filter(&letter?/1)
+    |> Enum.frequencies()
+    |> tap(fn _ -> StringIO.close(pid) end)
+  end
+
+  defp sum_frequencies(f1, f2), do: Map.merge(f1, f2, fn _, v1, v2 -> v1 + v2 end)
+
+  defp letter?(downcase), do: downcase != String.upcase(downcase)
 end
