69099153832930b79679c75b90ca1439bd12a733
aifrak <aifrak@users.noreply.github.com>
2022-08-13 08:28:42 +0000

    Download "Phone Number"

---
+++ elixir/phone-number/lib/phone_number.ex
@@ -0,0 +1,8 @@
+defmodule PhoneNumber do
+  @doc """
+  Remove formatting from a phone number if the given number is valid. Return an error otherwise.
+  """
+  @spec clean(String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  def clean(raw) do
+  end
+end
