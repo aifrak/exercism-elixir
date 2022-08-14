f0702414de7ec5314af134cf93447ca2f194c0c2
aifrak <aifrak@users.noreply.github.com>
2022-08-14 09:47:40 +0000

    Complete "Rational Numbers"

--- elixir/rational-numbers/lib/rational_numbers.ex
+++ elixir/rational-numbers/lib/rational_numbers.ex
@@ -5,55 +5,63 @@ defmodule RationalNumbers do
   Add two rational numbers
   """
   @spec add(a :: rational, b :: rational) :: rational
-  def add(a, b) do
-  end
+  def add({a1, b1}, {a2, b2}), do: {a1 * b2 + a2 * b1, b1 * b2} |> reduce()
 
   @doc """
   Subtract two rational numbers
   """
   @spec subtract(a :: rational, b :: rational) :: rational
-  def subtract(a, b) do
-  end
+  def subtract({a1, b1}, {a2, b2}), do: {a1 * b2 - a2 * b1, b1 * b2} |> reduce()
 
   @doc """
   Multiply two rational numbers
   """
   @spec multiply(a :: rational, b :: rational) :: rational
-  def multiply(a, b) do
-  end
+  def multiply({a1, b1}, {a2, b2}), do: {a1 * a2, b1 * b2} |> reduce()
 
   @doc """
   Divide two rational numbers
   """
   @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by(num, den) do
-  end
+  def divide_by({a1, b1}, {a2, b2}), do: {a1 * b2, a2 * b1} |> reduce()
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs(a) do
-  end
+  def abs({a, b}), do: {Kernel.abs(a), Kernel.abs(b)} |> reduce()
 
   @doc """
   Exponentiation of a rational number by an integer
   """
   @spec pow_rational(a :: rational, n :: integer) :: rational
-  def pow_rational(a, n) do
-  end
+  def pow_rational({a, b}, n) when n < 0,
+    do: Kernel.abs(n) |> then(&{b ** &1, a ** &1}) |> reduce()
+
+  def pow_rational({a, b}, n), do: {a ** n, b ** n} |> reduce()
 
   @doc """
   Exponentiation of a real number by a rational number
   """
   @spec pow_real(x :: integer, n :: rational) :: float
-  def pow_real(x, n) do
-  end
+  def pow_real(x, {a, b}), do: nth_root(x ** a, b)
+
+  defp nth_root(p, q), do: p ** (1 / q)
 
   @doc """
   Reduce a rational number to its lowest terms
   """
   @spec reduce(a :: rational) :: rational
-  def reduce(a) do
+  def reduce({a1, b1} = a) do
+    gcd = Integer.gcd(a1, b1)
+
+    if gcd == 1 do
+      standard_form(a)
+    else
+      reduce({div(a1, gcd), div(b1, gcd)})
+    end
   end
+
+  defp standard_form({a1, b1}) when b1 < 0, do: {a1 * -1, b1 * -1}
+  defp standard_form(a), do: a
 end
