4f30a320beba129efc0c56b769b74e41ca3db306
aifrak <aifrak@users.noreply.github.com>
2022-08-16 09:39:20 +0000

    Download "Spiral Matrix"

---
+++ elixir/spiral-matrix/lib/spiral.ex
@@ -0,0 +1,8 @@
+defmodule Spiral do
+  @doc """
+  Given the dimension, return a square matrix of numbers in clockwise spiral order.
+  """
+  @spec matrix(dimension :: integer) :: list(list(integer))
+  def matrix(dimension) do
+  end
+end
