5ca83aa50381cfd493cb33aeb2310f533eb92540
aifrak <aifrak@users.noreply.github.com>
2022-08-23 11:36:45 +0000

    Complete "Clock"

--- elixir/clock/lib/clock.ex
+++ elixir/clock/lib/clock.ex
@@ -1,14 +1,19 @@
 defmodule Clock do
   defstruct hour: 0, minute: 0
 
+  @type t :: %__MODULE__{hour: integer, minute: integer}
+
   @doc """
   Returns a clock that can be represented as a string:
 
       iex> Clock.new(8, 9) |> to_string
       "08:09"
   """
-  @spec new(integer, integer) :: Clock
+  @spec new(integer, integer) :: Clock.t()
   def new(hour, minute) do
+    m_clock = to_clock(minute)
+
+    %__MODULE__{hour: Integer.mod(hour + m_clock.hour, 24), minute: m_clock.minute}
   end
 
   @doc """
@@ -17,7 +22,16 @@ defmodule Clock do
       iex> Clock.new(10, 0) |> Clock.add(3) |> to_string
       "10:03"
   """
-  @spec add(Clock, integer) :: Clock
-  def add(%Clock{hour: hour, minute: minute}, add_minute) do
-  end
+  @spec add(Clock.t(), integer()) :: Clock.t()
+  def add(%Clock{hour: hour, minute: minute}, add_minute), do: new(hour, minute + add_minute)
+
+  defp to_clock(minute),
+    do: %__MODULE__{hour: Integer.floor_div(minute, 60), minute: Integer.mod(minute, 60)}
+end
+
+defimpl String.Chars, for: Clock do
+  @spec to_string(Clock.t()) :: String.t()
+  def to_string(%Clock{hour: hour, minute: minute}), do: "#{print(hour)}:#{print(minute)}"
+
+  defp print(number), do: number |> Integer.to_string() |> String.pad_leading(2, "0")
 end
