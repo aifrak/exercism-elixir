7ec6b6db9625a05b38c8e017c3c6a9b82151514b
aifrak <aifrak@users.noreply.github.com>
2022-08-23 08:06:55 +0000

    Download "Clock"

---
+++ elixir/clock/lib/clock.ex
@@ -0,0 +1,23 @@
+defmodule Clock do
+  defstruct hour: 0, minute: 0
+
+  @doc """
+  Returns a clock that can be represented as a string:
+
+      iex> Clock.new(8, 9) |> to_string
+      "08:09"
+  """
+  @spec new(integer, integer) :: Clock
+  def new(hour, minute) do
+  end
+
+  @doc """
+  Adds two clock times:
+
+      iex> Clock.new(10, 0) |> Clock.add(3) |> to_string
+      "10:03"
+  """
+  @spec add(Clock, integer) :: Clock
+  def add(%Clock{hour: hour, minute: minute}, add_minute) do
+  end
+end
