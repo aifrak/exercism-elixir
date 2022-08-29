4403d65fd94de0d1697e969e4a9aabe163ec0774
aifrak <aifrak@users.noreply.github.com>
2022-08-29 08:11:17 +0000

    Download "Pythagorean Triplet"

---
+++ elixir/pythagorean-triplet/lib/triplet.ex
@@ -0,0 +1,29 @@
+defmodule Triplet do
+  @doc """
+  Calculates sum of a given triplet of integers.
+  """
+  @spec sum([non_neg_integer]) :: non_neg_integer
+  def sum(triplet) do
+  end
+
+  @doc """
+  Calculates product of a given triplet of integers.
+  """
+  @spec product([non_neg_integer]) :: non_neg_integer
+  def product(triplet) do
+  end
+
+  @doc """
+  Determines if a given triplet is pythagorean. That is, do the squares of a and b add up to the square of c?
+  """
+  @spec pythagorean?([non_neg_integer]) :: boolean
+  def pythagorean?([a, b, c]) do
+  end
+
+  @doc """
+  Generates a list of pythagorean triplets whose values add up to a given sum.
+  """
+  @spec generate(non_neg_integer) :: [list(non_neg_integer)]
+  def generate(sum) do
+  end
+end
