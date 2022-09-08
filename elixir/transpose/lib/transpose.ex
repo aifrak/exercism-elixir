f68e1c871ed86fbf2cad0fe957395075dfcefb60
aifrak <aifrak@users.noreply.github.com>
2022-09-08 10:57:52 +0000

    Complete "Transpose"

--- elixir/transpose/lib/transpose.ex
+++ elixir/transpose/lib/transpose.ex
@@ -17,7 +17,24 @@ defmodule Transpose do
     "AD\\nBE\\n F"
   """
 
+  @pad "\t"
+
   @spec transpose(String.t()) :: String.t()
   def transpose(input) do
+    list = String.split(input, "\n")
+    padding = padding(list)
+
+    list
+    |> Enum.map(&string_to_row(&1, padding))
+    |> Enum.zip_with(& &1)
+    |> Enum.map_join("\n", &row_to_string/1)
   end
+
+  defp padding(list), do: list |> Enum.max_by(&String.length/1) |> String.length()
+
+  defp string_to_row(string, padding),
+    do: string |> String.pad_trailing(padding, @pad) |> String.graphemes()
+
+  defp row_to_string(row),
+    do: row |> Enum.join() |> String.trim_trailing(@pad) |> String.replace(@pad, " ")
 end
