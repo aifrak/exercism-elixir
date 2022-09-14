d31a72d76caccb7dc40017d4eb0066db469dd7a1
aifrak <aifrak@users.noreply.github.com>
2022-09-14 15:06:35 +0000

    Complete "Knapsack"

--- elixir/knapsack/lib/knapsack.ex
+++ elixir/knapsack/lib/knapsack.ex
@@ -4,6 +4,11 @@ defmodule Knapsack do
   """
   @spec maximum_value(items :: [%{value: integer, weight: integer}], maximum_weight :: integer) ::
           integer
-  def maximum_value(items, maximum_weight) do
-  end
+  def maximum_value([], _), do: 0
+
+  def maximum_value([hd | tl], maximum_weight) when hd.weight > maximum_weight,
+    do: maximum_value(tl, maximum_weight)
+
+  def maximum_value([%{value: value, weight: weight} | tl], maximum_weight),
+    do: max(value + maximum_value(tl, maximum_weight - weight), maximum_value(tl, maximum_weight))
 end
