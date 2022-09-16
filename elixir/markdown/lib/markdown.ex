3253ba2b51bc8b312ab825f4ed4a9b113527ddcf
aifrak <aifrak@users.noreply.github.com>
2022-09-16 08:17:01 +0000

    Download "Markdown"

---
+++ elixir/markdown/lib/markdown.ex
@@ -0,0 +1,79 @@
+defmodule Markdown do
+  @doc """
+    Parses a given string with Markdown syntax and returns the associated HTML for that string.
+
+    ## Examples
+
+      iex> Markdown.parse("This is a paragraph")
+      "<p>This is a paragraph</p>"
+
+      iex> Markdown.parse("# Header!\\n* __Bold Item__\\n* _Italic Item_")
+      "<h1>Header!</h1><ul><li><strong>Bold Item</strong></li><li><em>Italic Item</em></li></ul>"
+  """
+  @spec parse(String.t()) :: String.t()
+  def parse(m) do
+    patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
+  end
+
+  defp process(t) do
+    if (String.starts_with?(t, "#") && !String.starts_with?(t, "#######")) ||
+         String.starts_with?(t, "*") do
+      if String.starts_with?(t, "#") do
+        enclose_with_header_tag(parse_header_md_level(t))
+      else
+        parse_list_md_level(t)
+      end
+    else
+      enclose_with_paragraph_tag(String.split(t))
+    end
+  end
+
+  defp parse_header_md_level(hwt) do
+    [h | t] = String.split(hwt)
+    {to_string(String.length(h)), Enum.join(t, " ")}
+  end
+
+  defp parse_list_md_level(l) do
+    t = String.split(String.trim_leading(l, "* "))
+    "<li>" <> join_words_with_tags(t) <> "</li>"
+  end
+
+  defp enclose_with_header_tag({hl, htl}) do
+    "<h" <> hl <> ">" <> htl <> "</h" <> hl <> ">"
+  end
+
+  defp enclose_with_paragraph_tag(t) do
+    "<p>#{join_words_with_tags(t)}</p>"
+  end
+
+  defp join_words_with_tags(t) do
+    Enum.join(Enum.map(t, fn w -> replace_md_with_tag(w) end), " ")
+  end
+
+  defp replace_md_with_tag(w) do
+    replace_suffix_md(replace_prefix_md(w))
+  end
+
+  defp replace_prefix_md(w) do
+    cond do
+      w =~ ~r/^#{"__"}{1}/ -> String.replace(w, ~r/^#{"__"}{1}/, "<strong>", global: false)
+      w =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(w, ~r/_/, "<em>", global: false)
+      true -> w
+    end
+  end
+
+  defp replace_suffix_md(w) do
+    cond do
+      w =~ ~r/#{"__"}{1}$/ -> String.replace(w, ~r/#{"__"}{1}$/, "</strong>")
+      w =~ ~r/[^#{"_"}{1}]/ -> String.replace(w, ~r/_/, "</em>")
+      true -> w
+    end
+  end
+
+  defp patch(l) do
+    String.replace(l, "<li>", "<ul>" <> "<li>", global: false)
+    |> String.reverse()
+    |> String.replace(String.reverse("</li>"), String.reverse("</li></ul>"), global: false)
+    |> String.reverse()
+  end
+end
