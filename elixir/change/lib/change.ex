91702bb04a52cb5245fdc67bcac215bf9cf8ee18
aifrak <aifrak@users.noreply.github.com>
2022-10-13 08:44:32 +0000

    Download "Change"

---
+++ elixir/change/lib/change.ex
@@ -0,0 +1,21 @@
+defmodule Change do
+  @doc """
+    Determine the least number of coins to be given to the user such
+    that the sum of the coins' value would equal the correct amount of change.
+    It returns {:error, "cannot change"} if it is not possible to compute the
+    right amount of coins. Otherwise returns the tuple {:ok, list_of_coins}
+
+    ## Examples
+
+      iex> Change.generate([5, 10, 15], 3)
+      {:error, "cannot change"}
+
+      iex> Change.generate([1, 5, 10], 18)
+      {:ok, [1, 1, 1, 5, 10]}
+
+  """
+
+  @spec generate(list, integer) :: {:ok, list} | {:error, String.t()}
+  def generate(coins, target) do
+  end
+end
