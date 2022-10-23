149eafaaebb4ae07c6a09ab3ba33eef6b7c932dc
aifrak <aifrak@users.noreply.github.com>
2022-10-23 09:19:52 +0000

    Download "Queen Attack"

---
+++ elixir/queen-attack/lib/queens.ex
@@ -0,0 +1,26 @@
+defmodule Queens do
+  @type t :: %Queens{black: {integer, integer}, white: {integer, integer}}
+  defstruct [:white, :black]
+
+  @doc """
+  Creates a new set of Queens
+  """
+  @spec new(Keyword.t()) :: Queens.t()
+  def new(opts \\ []) do
+  end
+
+  @doc """
+  Gives a string representation of the board with
+  white and black queen locations shown
+  """
+  @spec to_string(Queens.t()) :: String.t()
+  def to_string(queens) do
+  end
+
+  @doc """
+  Checks if the queens can attack each other
+  """
+  @spec can_attack?(Queens.t()) :: boolean
+  def can_attack?(queens) do
+  end
+end
