1675e6e97491b91c35cbc690fedebc640f4ab00d
aifrak <aifrak@users.noreply.github.com>
2022-11-08 19:25:15 +0000

    Complete "DOT DSL"

--- elixir/dot-dsl/lib/dot.ex
+++ elixir/dot-dsl/lib/dot.ex
@@ -1,4 +1,24 @@
 defmodule Dot do
-  defmacro graph(ast) do
+  defmacro graph(ast),
+    do: ast |> Macro.prewalk(Graph.new(), &read_ast/2) |> elem(1) |> Macro.escape()
+
+  defp read_ast([do: ast], graph), do: read_ast(ast, graph)
+  defp read_ast({:__block__, _, _} = ast, graph), do: {ast, graph}
+  defp read_ast({:graph, _, [attrs]}, graph), do: {nil, Graph.put_attrs(graph, attrs)}
+
+  defp read_ast({:--, _, [{from, _, _}, {to, _, nil}]}, graph),
+    do: {nil, Graph.add_edge(graph, from, to)}
+
+  defp read_ast({:--, _, [{from, _, _}, {to, _, [attrs]}]}, graph),
+    do: {nil, Graph.add_edge(graph, from, to, attrs)}
+
+  defp read_ast({marker, _, nil}, graph), do: {nil, Graph.add_node(graph, marker)}
+
+  defp read_ast({marker, _, [attrs]}, graph) when is_list(attrs) do
+    if Keyword.keyword?(attrs),
+      do: {nil, Graph.add_node(graph, marker, attrs)},
+      else: raise(ArgumentError)
   end
+
+  defp read_ast(_, _), do: raise(ArgumentError)
 end
