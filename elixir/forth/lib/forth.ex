7e4e94cb0660f7ddfbbb50be873805669ad66a6e
aifrak <aifrak@users.noreply.github.com>
2022-12-12 09:15:32 +0000

    Download "Forth"

---
+++ elixir/forth/lib/forth.ex
@@ -0,0 +1,45 @@
+defmodule Forth do
+  @opaque evaluator :: any
+
+  @doc """
+  Create a new evaluator.
+  """
+  @spec new() :: evaluator
+  def new() do
+  end
+
+  @doc """
+  Evaluate an input string, updating the evaluator state.
+  """
+  @spec eval(evaluator, String.t()) :: evaluator
+  def eval(ev, s) do
+  end
+
+  @doc """
+  Return the current stack as a string with the element on top of the stack
+  being the rightmost element in the string.
+  """
+  @spec format_stack(evaluator) :: String.t()
+  def format_stack(ev) do
+  end
+
+  defmodule StackUnderflow do
+    defexception []
+    def message(_), do: "stack underflow"
+  end
+
+  defmodule InvalidWord do
+    defexception word: nil
+    def message(e), do: "invalid word: #{inspect(e.word)}"
+  end
+
+  defmodule UnknownWord do
+    defexception word: nil
+    def message(e), do: "unknown word: #{inspect(e.word)}"
+  end
+
+  defmodule DivisionByZero do
+    defexception []
+    def message(_), do: "division by zero"
+  end
+end
