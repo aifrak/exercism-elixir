d2bf44880e8254be84f8dfca4e3c7fba680517c6
aifrak <aifrak@users.noreply.github.com>
2023-01-28 18:31:55 +0000

    Improve Hamming

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -1,6 +1,4 @@
 defmodule Hamming do
-  defguardp same_length(strand1, strand2) when length(strand1) == length(strand2)
-
   @doc """
   Returns number of differences between two strands of DNA, known as the Hamming Distance.
 
@@ -10,18 +8,16 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
-
-  def hamming_distance(strand1, strand2) when same_length(strand1, strand2),
-    do: {:ok, do_hamming_distance(strand1, strand2)}
-
-  def hamming_distance(_, _), do: {:error, "strands must be of equal length"}
+  def hamming_distance(strand1, strand2), do: do_hamming_distance(strand1, strand2)
 
   defp do_hamming_distance(strand1, strand2, distance \\ 0)
-  defp do_hamming_distance([], [], distance), do: distance
+  defp do_hamming_distance([], [], distance), do: {:ok, distance}
 
   defp do_hamming_distance([letter1 | tail1], [letter1 | tail2], distance),
     do: do_hamming_distance(tail1, tail2, distance)
 
   defp do_hamming_distance([_ | tail1], [_ | tail2], distance),
     do: do_hamming_distance(tail1, tail2, distance + 1)
+
+  defp do_hamming_distance(_, _, _), do: {:error, "strands must be of equal length"}
 end
