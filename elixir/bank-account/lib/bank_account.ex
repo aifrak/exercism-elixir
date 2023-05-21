366ed30659e2d5e02a81b326c2b76548fff6baa7
aifrak <aifrak@users.noreply.github.com>
2023-05-21 13:57:17 +0000

    Bank Account: Keep in sync and solve failed tests

--- elixir/bank-account/lib/bank_account.ex
+++ elixir/bank-account/lib/bank_account.ex
@@ -9,37 +9,75 @@ defmodule BankAccount do
   @opaque account :: pid
 
   @doc """
-  Open the bank. Makes the account available.
+  Open the bank account, making it available for further operations.
   """
-  @spec open_bank() :: account
-  def open_bank() do
+  @spec open() :: account
+  def open() do
     {:ok, account} = Agent.start_link(fn -> 0 end)
     account
   end
 
   @doc """
-  Close the bank. Makes the account unavailable.
+  Close the bank account, making it unavailable for further operations.
   """
-  @spec close_bank(account) :: none
-  def close_bank(account), do: Agent.stop(account)
+  @spec close(account) :: :ok
+  def close(account), do: Agent.stop(account)
 
   @doc """
   Get the account's balance.
   """
-  @spec balance(account) :: integer
+  @spec balance(account) :: integer | {:error, :amount_must_be_positive}
   def balance(account) do
-    if Process.alive?(account),
-      do: Agent.get(account, & &1),
-      else: {:error, :account_closed}
+    with :ok <- validate_opened_account(account) do
+      do_balance(account)
+    else
+      error -> error
+    end
   end
 
   @doc """
-  Update the account's balance by adding the given amount which may be negative.
+  Add the given amount to the account's balance.
   """
-  @spec update(account, integer) :: any
-  def update(account, amount) do
-    if Process.alive?(account),
-      do: Agent.update(account, &(&1 + amount)),
-      else: {:error, :account_closed}
+  @spec deposit(account, integer) :: :ok | {:error, :account_closed | :amount_must_be_positive}
+  def deposit(account, amount) do
+    with :ok <- validate_amount(amount),
+         :ok <- validate_opened_account(account) do
+      do_deposit(account, amount)
+    else
+      error -> error
+    end
+  end
+
+  @doc """
+  Subtract the given amount from the account's balance.
+  """
+  @spec withdraw(account, integer) ::
+          :ok | {:error, :account_closed | :amount_must_be_positive | :not_enough_balance}
+  def withdraw(account, amount) do
+    with :ok <- validate_amount(amount),
+         balance when is_integer(balance) <- balance(account),
+         :ok <- validate_withdraw(balance, amount) do
+      do_withdraw(account, amount)
+    else
+      error -> error
+    end
+  end
+
+  defp do_balance(account), do: Agent.get(account, & &1)
+
+  defp do_deposit(account, amount), do: Agent.update(account, &(&1 + amount))
+
+  defp do_withdraw(account, amount), do: Agent.update(account, &(&1 - amount))
+
+  defp validate_opened_account(account) do
+    if Process.alive?(account), do: :ok, else: {:error, :account_closed}
+  end
+
+  defp validate_amount(amount) do
+    if amount >= 0, do: :ok, else: {:error, :amount_must_be_positive}
+  end
+
+  defp validate_withdraw(balance, amount) do
+    if balance - amount >= 0, do: :ok, else: {:error, :not_enough_balance}
   end
 end
