7fd75907a566bea86217748ce169d2af62e5268f
aifrak <aifrak@users.noreply.github.com>
2023-05-26 09:17:47 +0000

    Rational Numbers: Improve code

--- elixir/rational-numbers/lib/rational_numbers.ex
+++ elixir/rational-numbers/lib/rational_numbers.ex
@@ -5,40 +5,40 @@ defmodule RationalNumbers do
   Add two rational numbers
   """
   @spec add(a :: rational, b :: rational) :: rational
-  def add({a1, b1}, {a2, b2}), do: {a1 * b2 + a2 * b1, b1 * b2} |> reduce()
+  def add({a1, b1}, {a2, b2}), do: reduce({a1 * b2 + a2 * b1, b1 * b2})
 
   @doc """
   Subtract two rational numbers
   """
   @spec subtract(a :: rational, b :: rational) :: rational
-  def subtract({a1, b1}, {a2, b2}), do: {a1 * b2 - a2 * b1, b1 * b2} |> reduce()
+  def subtract({a1, b1}, {a2, b2}), do: reduce({a1 * b2 - a2 * b1, b1 * b2})
 
   @doc """
   Multiply two rational numbers
   """
   @spec multiply(a :: rational, b :: rational) :: rational
-  def multiply({a1, b1}, {a2, b2}), do: {a1 * a2, b1 * b2} |> reduce()
+  def multiply({a1, b1}, {a2, b2}), do: reduce({a1 * a2, b1 * b2})
 
   @doc """
   Divide two rational numbers
   """
   @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by({a1, b1}, {a2, b2}), do: {a1 * b2, a2 * b1} |> reduce()
+  def divide_by({a1, b1}, {a2, b2}), do: reduce({a1 * b2, a2 * b1})
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs({a, b}), do: {Kernel.abs(a), Kernel.abs(b)} |> reduce()
+  def abs({a, b}), do: reduce({Kernel.abs(a), Kernel.abs(b)})
 
   @doc """
   Exponentiation of a rational number by an integer
   """
   @spec pow_rational(a :: rational, n :: integer) :: rational
   def pow_rational({a, b}, n) when n < 0,
-    do: Kernel.abs(n) |> then(&{b ** &1, a ** &1}) |> reduce()
+    do: n |> Kernel.abs() |> then(&{b ** &1, a ** &1}) |> reduce()
 
-  def pow_rational({a, b}, n), do: {a ** n, b ** n} |> reduce()
+  def pow_rational({a, b}, n), do: reduce({a ** n, b ** n})
 
   @doc """
   Exponentiation of a real number by a rational number
@@ -55,11 +55,9 @@ defmodule RationalNumbers do
   def reduce({a1, b1} = a) do
     gcd = Integer.gcd(a1, b1)
 
-    if gcd == 1 do
-      standard_form(a)
-    else
-      reduce({div(a1, gcd), div(b1, gcd)})
-    end
+    if gcd == 1,
+      do: standard_form(a),
+      else: reduce({div(a1, gcd), div(b1, gcd)})
   end
 
   defp standard_form({a1, b1}) when b1 < 0, do: {a1 * -1, b1 * -1}
