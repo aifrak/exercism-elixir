b84fba7bf5bd6a36de4af0c8b6b8469cd71ae9af
aifrak <aifrak@users.noreply.github.com>
2023-05-26 13:58:23 +0000

    Poker: Use words sigil

--- elixir/poker/lib/poker.ex
+++ elixir/poker/lib/poker.ex
@@ -1,5 +1,5 @@
 defmodule Poker do
-  @ranks ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+  @ranks ~w(2 3 4 5 6 7 8 9 10 J Q K A)
          |> Enum.with_index(2)
          |> Map.new()
 
