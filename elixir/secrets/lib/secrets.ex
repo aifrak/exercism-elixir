597a28eb127fa2064fcb6567b601a4c4ab28c084
aifrak <aifrak@users.noreply.github.com>
2023-05-31 09:38:09 +0000

    Secrets: Make code shorter

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,35 +1,16 @@
 defmodule Secrets do
-  use Bitwise
+  def secret_add(secret), do: &Kernel.+(&1, secret)
 
-  def secret_add(secret) do
-    fn value -> value + secret end
-  end
+  def secret_subtract(secret), do: &Kernel.-(&1, secret)
 
-  def secret_subtract(secret) do
-    fn value -> value - secret end
-  end
+  def secret_multiply(secret), do: &Kernel.*(&1, secret)
 
-  def secret_multiply(secret) do
-    fn value -> value * secret end
-  end
+  def secret_divide(secret), do: &div(&1, secret)
 
-  def secret_divide(secret) do
-    fn value -> div(value, secret) end
-  end
+  def secret_and(secret), do: &Bitwise.&&&(&1, secret)
 
-  def secret_and(secret) do
-    fn value -> value &&& secret end
-  end
+  def secret_xor(secret), do: &Bitwise.bxor(&1, secret)
 
-  def secret_xor(secret) do
-    fn value -> bxor(value, secret) end
-  end
-
-  def secret_combine(secret_function1, secret_function2) do
-    fn value ->
-      value
-      |> secret_function1.()
-      |> secret_function2.()
-    end
-  end
+  def secret_combine(secret_function1, secret_function2),
+    do: &(&1 |> secret_function1.() |> secret_function2.())
 end
