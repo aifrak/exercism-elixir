e92ddbe1c6b7f9d9a90ba365c9e9602eda85c8e1
aifrak <aifrak@users.noreply.github.com>
2023-06-24 19:24:38 +0000

    Safer setup of Exercism

--- .devcontainer/setup-dev-env.sh
+++ .devcontainer/setup-dev-env.sh
@@ -2,17 +2,21 @@
 
 set -euo pipefail
 
-env_file="${APP_DIR}/.env"
+env_file="$APP_DIR/.env"
 
-# Export all environment variables
-if [[ -f "$env_file" ]]; then
+if [[ ! -f $env_file ]]; then
+  echo "WARNING: .env file missing"
+elif [[ -z $EXERCISM_TOKEN ]]; then
+  echo "WARNING: EXERCISM_TOKEN is empty"
+else
+  # Export all environment variables
   set -a
   # shellcheck source="../.env"
   . "$env_file"
   set +a
 
   # Configure Exercism
-  exercism configure --token "$EXERCISM_TOKEN" --workspace "$APP_DIR"
-else
-  echo "WARNING: .env file missing"
+  exercism configure --token $EXERCISM_TOKEN --workspace "$APP_DIR" \
+    &>/dev/null &&
+    echo "Exercism configured successfully."
 fi
