76cccf59e3ac0b2bf86c080290042ae5b8eb937e
aifrak <aifrak@users.noreply.github.com>
2023-10-28 10:23:33 +0000

    Bottle Song: Complete exercise

--- elixir/bottle-song/lib/bottle_song.ex
+++ elixir/bottle-song/lib/bottle_song.ex
@@ -4,6 +4,40 @@ defmodule BottleSong do
   """
 
   @spec recite(pos_integer, pos_integer) :: String.t()
-  def recite(_start_bottle, _take_down) do
+  def recite(start_bottle, take_down) do
+    start_bottle..1
+    |> Enum.take(take_down)
+    |> Enum.map_join("\n\n", &verse/1)
   end
+
+  defp verse(number) do
+    count = number |> count() |> String.capitalize()
+    bottles = bottles(number)
+
+    previous_count = count(number - 1)
+    previous_bottles = bottles(number - 1)
+
+    """
+    #{count} green #{bottles} hanging on the wall,
+    #{count} green #{bottles} hanging on the wall,
+    And if one green bottle should accidentally fall,
+    There'll be #{previous_count} green #{previous_bottles} hanging on the wall.\
+    """
+  end
+
+  defp count(0), do: "no"
+  defp count(1), do: "one"
+  defp count(2), do: "two"
+  defp count(3), do: "three"
+  defp count(4), do: "four"
+  defp count(5), do: "five"
+  defp count(6), do: "six"
+  defp count(7), do: "seven"
+  defp count(8), do: "eight"
+  defp count(9), do: "nine"
+  defp count(10), do: "ten"
+
+  defp bottles(0), do: "bottles"
+  defp bottles(1), do: "bottle"
+  defp bottles(count) when count > 1, do: "bottles"
 end
